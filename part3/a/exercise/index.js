03f0397a7fd49649c018a88d08990227cc8f48c5
zuzhi <zuzhi.hu@gmail.com>
2024-05-17 10:48:51 +0800

    first commit, part1, part3 a with exercise

---
+++ part3/a/exercise/index.js
@@ -0,0 +1,124 @@
+const express = require('express')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(express.json())
+
+morgan.token('body', (req, res) => {
+  return JSON.stringify(req.body)
+})
+
+//app.use(morgan('tiny'))
+//app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    tokens.method(req, res) === 'POST'
+      ? tokens.body(req, res)
+      : ''
+  ].join(' ')
+}))
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456"
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523"
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345"
+  },
+  {
+    id: 4,
+    name: "Mary Poppendick",
+    number: "39-23-6423122"
+  }
+]
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+  response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${""+(new Date())}</p>`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(p => p.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(p => p.id !== id)
+
+  response.status(204).end()
+})
+
+const generateId = () => {
+  return Math.floor(Math.random() * 10000000)
+}
+
+const nameExist = (name) => {
+  return persons.filter(p => p.name === name).length > 0 || false
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'name missing'
+    })
+  }
+
+  if (!body.number) {
+    return response.status(400).json({
+      error: 'number missing'
+    })
+  }
+
+  if (nameExist(body.name)) {
+    return response.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number
+  }
+
+  persons = persons.concat(person)
+
+  response.json(persons)
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
