89539a7ce73f12af2816807fee34a2fc8741d489
zuzhi <zuzhi.hu@gmail.com>
2024-06-28 20:18:02 +0800

    refactor the application into separate modules

--- part3/a/backend/mongo.js
+++ part3/a/backend/mongo.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
+const logger = require('logger')
 
 if (process.argv.length<3) {
-  console.log('give password as argument')
+  logger.info('give password as argument')
   process.exit(1)
 }
 
@@ -23,7 +24,7 @@ const Note = mongoose.model('Note', noteSchema)
 
 Note.find({}).then(result => {
   result.forEach(note => {
-    console.log(note)
+    logger.info(note)
   })
   mongoose.connection.close()
 })
